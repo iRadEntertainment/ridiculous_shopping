8f2cf672715948055bda138e90e00a0976b80a81
konradgryt <konrad.gryt@gmail.com>
2025-05-26 02:15:54 +0200

    [audio, ui, bugfix] audio options slider now functional

--- Singletons/Aud.gd
+++ Singletons/Aud.gd
@@ -2,8 +2,27 @@
 extends Node
 
 const MASTER_BUS_INDEX := 0
+const DEFAULT_VOLUME: float = -2
 
 var is_game_muted := false
+var master_db_volume := DEFAULT_VOLUME
+
+func _ready() -> void:
+	load_audio_settings()
+	
+func load_audio_settings() -> void:
+	var config = ConfigFile.new()
+
+	if config.load("user://audio_settings.cfg") == OK:
+		master_db_volume = config.get_value("audio", "Master", DEFAULT_VOLUME)
+	else:
+		master_db_volume = DEFAULT_VOLUME
+
+func save_audio_settings() -> void:
+	var config = ConfigFile.new()
+	config.set_value("audio", "Master", master_db_volume)
+	
+	config.save("user://audio_settings.cfg")
 
 func play_main_theme_music() -> void:
 	%MainStreamPlayer.play()
--- ui/options.gd
+++ ui/options.gd
@@ -4,10 +4,12 @@ extends Node
 func _ready() -> void:
 	self.process_mode = Node.PROCESS_MODE_ALWAYS
 	
-	%Volume.value = db_to_linear(2)
+	%Volume.value = db_to_linear(Aud.master_db_volume)
 
 func _on_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(Aud.MASTER_BUS_INDEX, value)
+	Aud.master_db_volume = linear_to_db(value)
+	AudioServer.set_bus_volume_db(Aud.MASTER_BUS_INDEX, Aud.master_db_volume)
+	Aud.save_audio_settings()
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/MainMenu.tscn")
