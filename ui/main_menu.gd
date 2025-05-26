c527614b2aa9cd44a132e946ff03ac7a637c4a22
Dario De Vita <devitadario@gmail.com>
2025-05-26 02:43:36 +0200

    feat: added game theme

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -11,7 +11,7 @@ func _on_start_pressed() -> void:
 	
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/Options.tscn")
-	
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
